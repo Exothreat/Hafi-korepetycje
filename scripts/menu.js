09a071ef75ba777a33c20e244d57fcff32f53ad8
Norbert Prucnal <nor.bert.prucnal@gmail.com>
2023-10-24 20:37:58 +0200

    small change to scrollbar

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,15 +1,18 @@
 const openMenu = document.querySelector(".open-menu");
 const overlay = document.querySelector(".overlay");
 const menu = document.querySelector(".menu");
+const body = document.querySelector(".body");
 
 openMenu.addEventListener("click", () => {
   openMenu.style.left = "-50px";
   overlay.style.display = "block";
   menu.style.left = "0px";
+  body.style.overflow = "hidden";
 });
 
 overlay.addEventListener("click", () => {
   openMenu.style.left = "0px";
   overlay.style.display = "none";
   menu.style.left = "-250px";
+  body.style.overflow = "visible";
 });
